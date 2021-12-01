c1ef4ec81a1a3b26955b9e237eea3cf56d8b331e
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2021-12-01 12:46:39 -0600

    Add part two

--- solutions/sonar_sweep.py
+++ solutions/sonar_sweep.py
@@ -1,4 +1,7 @@
 """
+Part One
+========
+
 The first order of business is to figure out how quickly the depth
 increases, just so you know what you're dealing with - you never know
 if the keys will get carried into deeper water by an ocean current or a
@@ -6,15 +9,21 @@ fish or something.
 
 To do this, count the number of times a depth measurement increases
 from the previous measurement.
+
+Part Two
+========
+Count the number of times the sum of measurements in this sliding
+window increases from the previous sum. So, compare A with B, then
+compare B with C, then C with D, and so on. Stop when there aren't
+enough measurements left to create a new three-measurement sum.
 """
 from typing import List
 
 
-def main(measurements: List[int]) -> int:
+def count_increases(measurements: List[int]) -> int:
     """Count the number of times the depth measurement increases."""
     total = 0
     past = 9999999  # Make the first comparison arbitrarily large.
-    # import ipdb; ipdb.set_trace()
     for measurement in measurements:
         if measurement > past:
             total += 1
@@ -23,8 +32,30 @@ def main(measurements: List[int]) -> int:
     return total
 
 
+def count_windows(measurements: List[int]) -> int:
+    """Count the number of times the total depth of a three-measurement
+    window increases.
+    """
+    total = 0
+    first = measurements[0]
+    second = measurements[1]
+    third = measurements[2]
+    past = sum([first, second, third])
+    for measurement in measurements[3:]:
+        first = second
+        second = third
+        third = measurement
+        new = sum([first, second, third])
+        if new > past:
+            total += 1
+        past = new
+
+    return total
+
+
 if __name__ == "__main__":
     with open("inputs/sonar_sweep.txt") as input:
         measurements = [int(measurement) for measurement in input]
 
-    print(f"Part One: {main(measurements)}")
+    print(f"Part One: {count_increases(measurements)}")
+    print(f"Part Two: {count_windows(measurements)}")
--- tests/test_sonar_sweep.py
+++ tests/test_sonar_sweep.py
@@ -1,17 +1,22 @@
-from solutions.sonar_sweep import main
+from solutions.sonar_sweep import count_increases, count_windows
 
 
 def test_increments_increases():
     """If the depth increases, increment."""
-    result = main([199, 200])
+    result = count_increases([199, 200])
     assert result == 1
 
 
 def test_skips_decreases():
     """If the depth decreases, skip."""
-    assert main([200, 199]) == 0
+    assert count_increases([200, 199]) == 0
 
 
 def test_sample():
     """Test the sample input."""
-    assert main([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 7
+    assert count_increases([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 7
+
+
+def test_part_two():
+    """Test part two with the sample input."""
+    assert count_windows([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 5
