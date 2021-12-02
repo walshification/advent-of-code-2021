87f1d0250701ace7018fea891303efd36546ea84
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2021-12-02 09:36:25 -0600

    Sub knows its final position

--- tests/test_dive.py
+++ tests/test_dive.py
@@ -19,3 +19,23 @@ def test_sub_moves_up():
     """Given a command to move up, the sub changes depth."""
     sub = Submarine()
     assert sub.engage("up 5").depth == -5
+
+
+def test_sub_follows_path_to_final_position_value():
+    """Sub can follow a path and calculate its final position's value.
+    """
+    sub = Submarine()
+    commands = [
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2",
+    ]
+    for command in commands:
+        sub.engage(command)
+
+    assert sub.horizontal_position == 15
+    assert sub.depth == 10
+    assert sub.final_value == 150
