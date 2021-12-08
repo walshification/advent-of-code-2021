0565e0fd1c0c1b1b686e69ed7982c272d708d37d
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2021-12-07 21:59:51 -0600

    Calculate total fuel needed for position

--- solutions/treachery_of_whales.py
+++ solutions/treachery_of_whales.py
@@ -4,7 +4,7 @@ least fuel possible. How much fuel must they spend to align to that
 position?
 """
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 
 
 @dataclass
@@ -12,9 +12,25 @@ class CrabFleet:
     """A collection of righteous crabs to protect you."""
 
     crab_positions: List[int]
+    _median: Optional[int] = None
 
     @property
     def median(self) -> int:
         """Returns the median of the crab positions."""
-        total = len(self.crab_positions)
-        return sorted(self.crab_positions)[total // 2]
+        if self._median is None:
+            total = len(self.crab_positions)
+            self._median = sorted(self.crab_positions)[total // 2]
+        return self._median
+
+    def calculate_minimum_fuel(self, position: int) -> int:
+        """Calculate the minimum-needed fuel to align to the median."""
+        total_fuel = 0
+        for position in self.crab_positions:
+            while not position == self.median:
+                if position > self.median:
+                    position -= 1
+                else:
+                    position += 1
+                total_fuel += 1
+
+        return total_fuel
--- tests/test_treachery_of_whales.py
+++ tests/test_treachery_of_whales.py
@@ -4,3 +4,17 @@ from solutions.treachery_of_whales import CrabFleet
 def test_crab_fleet_finds_median():
     """The crab fleet can find its median position of subs."""
     assert CrabFleet([0, 1, 1, 1, 5]).median == 1
+
+
+def test_crab_fleet_calculates_fuel():
+    """The crab fleet can find how much fuel it needs to get into
+    position.
+    """
+    fleet = CrabFleet([0, 1, 1, 1, 5])
+    assert fleet.calculate_minimum_fuel(fleet.median) == 5
+
+
+def test_part_one_input():
+    """Test the part one input."""
+    fleet = CrabFleet([16, 1, 2, 0, 4, 2, 7, 1, 2, 14])
+    assert fleet.calculate_minimum_fuel(fleet.median) == 37
